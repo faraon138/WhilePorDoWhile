ec1de2d2a3f2d073b5c66ba5f2ec1f89c5b38528
franciscofuentealba <franciscofuentealba@mbp-de-sergio>
2023-04-20 12:34:38 -0400

    desafio

---
+++ WhilePordoWhile/src/cl/faraon138/CambiarWhile.java
@@ -0,0 +1,29 @@
+//by faraon138
+
+package cl.faraon138;
+
+public class CambiarWhile {
+
+	public static void main(String[] args) {
+		
+		 int i = 0;
+		
+//			while (i<50) { i+=1;
+//			         System.out.printf("Iteración %d\n", i);
+//			      }
+//			} }
+		
+		
+do {
+	i+=1;
+	
+	 System.out.printf("Iteración %d\n", i);
+	
+} while(i<50);
+
+		
+		
+		
+		
+}
+}
